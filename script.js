e0d1ac8d6792302229c90cef40a5bf4ec82ac05e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-11 23:54:47 +0100

    new Error() added, nesting errors

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-const weekDay = (index) => {
-  const days = [
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-    "sunday",
-  ];
-  const day = days[index];
+{
+  const weekDay = (index) => {
+    const days = [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday",
+    ];
+    const day = days[index];
 
-  if (!day) {
-    throw "incorrect index";
-  }
+    if (!day) {
+      throw new Error("incorrect index");
+    }
 
-  return day;
-};
+    return day;
+  };
 
-try {
+  try {
     const day = weekDay(22);
     console.log(day);
-} catch (error) {
+  } catch (error) {
+    console.error(error.message);
+    console.error(error.name);
     console.error("You must enter a number between 0 and 6");
+  }
+}
+
+
+// nesting errors
+{
+    try{
+        try {
+            throw new Error("correct error message");
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
