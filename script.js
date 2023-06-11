2f28549fe98860f7fc13bca833ffc3625557faa9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-11 23:25:58 +0100

    throw, try, catch example

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const weekDay = (index) => {
+  const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  const day = days[index];
+
+  if (!day) {
+    throw "incorrect index";
+  }
+
+  return day;
+};
 
 try {
-    throw "Some serious error";
+    const day = weekDay(22);
+    console.log(day);
 } catch (error) {
-    console.error(error);
+    console.error("You must enter a number between 0 and 6");
 }
-
-// throw - serves to throw error
