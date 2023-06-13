c785794eb319e9e9b6e39127a8fab1803167d578
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-14 00:18:55 +0100

    XMLH, fetch, errors

--- scriptXMLH.js
+++ scriptXMLH.js
@@ -1,11 +1,56 @@
-const request = new HMLHttpRequest();
+// XMLH
 
-request.open("GET", "products.json");
+{
+  console.log("Loading 1:XMLH, 2:etch, 3:alternative fetch, 4:catch");
 
-request.responseType = "json";
+  const request = new XMLHttpRequest();
 
-request.onload = () => {
-  console, log(request.response);
-};
+  request.open("GET", "products.json");
 
-request.send();
+  request.responseType = "json";
+
+  request.onload = () => {
+    console.log(request.response);
+  };
+
+  request.send();
+}
+
+// fetch - replacement for XHR
+
+{
+  fetch("products.json").then((response) => {
+    response.json().then((products) => {
+      console.log(products);
+    });
+  });
+
+  // const promise = fetch("products.json");
+
+  // promise.then((response) => {
+  //   response.json().then((products) => {
+  //     console.log(products);
+  //   });
+  // });
+}
+
+{
+  //  alternative fetch
+
+  fetch("products.json")
+    .then((response) => response.json()) // first promise
+    .then((products) => console.log(products)); // second promise
+}
+
+{
+  // catch - when something goes wrong
+
+  fetch("https://thisdoesntexist")
+    .then((response) => response.json()) // first promise
+    .then((products) => console.log(products)) // second promise
+    .catch((error) => {
+      console.log("Something bad happened");
+      console.error(error);
+      alert("Something bad happened");
+    });
+}
