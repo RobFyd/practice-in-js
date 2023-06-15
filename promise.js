a954b8bbe572e89127f2448f4b9296b355fa61a0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-15 23:52:12 +0100

    better error handling for fetch

---
+++ promise.js
@@ -0,0 +1,27 @@
+// {
+//   const logProducts = (products) => console.log(products);
+
+//   const promise = fetch("products.json");
+
+//   promise
+//     .then((response) => response.json())
+//     .then(logProducts)
+//     .catch((error) => console.error("Something went wrong", error));
+// }
+
+{
+  fetch("products.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      return response;
+    })
+
+    .then((response) => response.json())
+
+    .then((response) => console.log(response))
+
+    .catch(() => console.error("Something went wrong"));
+}
