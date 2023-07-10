867763b58db5cda17dc1b469aae10caf0b3dec25
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-11 00:33:07 +0100

    generator exercises continued

--- generator.js
+++ generator.js
@@ -6,3 +6,33 @@ function* generator() {
 }
 
 const iterator  = generator();
+
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+
+function* generator2() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    return;  //iterator ends here
+    yield 6;
+    yield 7;
+    yield 8;
+}
+
+const iterator2 = generator2();
+
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
+console.log(iterator2.next().value);
