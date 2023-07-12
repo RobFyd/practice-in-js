ebb8c630b470b1b91238e72fc5d249f2d6a24d21
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-12 23:36:21 +0100

    more exercises with the generator

--- generator.js
+++ generator.js
@@ -1,38 +1,69 @@
-function* generator() {
+{
+  function* generator() {
     yield 5;
     yield "tekst";
     yield null;
-    yield {};         //between yields you can give other expressions
+    yield {}; //between yields you can give other expressions
+  }
+
+  const iterator = generator();
+
+  console.log(iterator.next().value);
+  console.log(iterator.next().value);
+  console.log(iterator.next().value);
+  console.log(iterator.next().value);
+  console.log(iterator.next().value);
+  console.log(iterator.next().value);
 }
 
-const iterator  = generator();
-
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-
-function* generator2() {
+{
+  function* generator2() {
     yield 1;
     yield 2;
     yield 3;
     yield 4;
     yield 5;
-    return;  //iterator ends here
+    return; //iterator ends here
     yield 6;
     yield 7;
     yield 8;
+  }
+
+  const iterator2 = generator2();
+
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
+  console.log(iterator2.next().value);
 }
 
-const iterator2 = generator2();
+function* generator3(parameter) {
+  console.log(parameter);
+  yield 5;
+  yield "tekst";
+  yield null;
+  yield {};
+}
 
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
-console.log(iterator2.next().value);
+const iterator3 = generator3(100);
+
+function* nextProductName(products) {
+  for (product of products) {
+    yield product.name;
+  }
+}
+
+const generator4 = nextProductName([
+  {
+    name: "Audio",
+    price: 299,
+  },
+  {
+    name: "TV",
+    price: 12999,
+  },
+]);
