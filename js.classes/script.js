97c0df2848de8ccaeb0a04d6bfd94386c0f785fc
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-10-17 00:41:38 +0100

    functions uncommented

--- js.classes/script.js
+++ js.classes/script.js
@@ -1,88 +1,94 @@
-// function getArea() {
-//     return this.width * this.height;
-// }
+{
+  function getArea() {
+    return this.width * this.height;
+  }
 
-// const rectangle1 = {
-//     width: 5,
-//     height: 6,
-//     getArea,
-// };
+  const rectangle1 = {
+    width: 5,
+    height: 6,
+    getArea,
+  };
 
-// const rectangle2 = {
-//     width: 7,
-//     height: 12,
-//     getArea,
-// };
+  const rectangle2 = {
+    width: 7,
+    height: 12,
+    getArea,
+  };
 
-// console.log(rectangle1);
-// console.log(rectangle2);
+  console.log(rectangle1);
+  console.log(rectangle2);
 
-// console.log(rectangle1.getArea());
-// console.log(rectangle2.getArea());
+  console.log(rectangle1.getArea());
+  console.log(rectangle2.getArea());
+}
 
 // ****************************************************
 
-// const getArea = ({ width, height }) => width * height;
+{
+  const getArea = ({ width, height }) => width * height;
 
-// const rectangle1 = {
-//     width: 5,
-//     height: 6,
-// };
+  const rectangle1 = {
+    width: 5,
+    height: 6,
+  };
 
-// const rectangle2 = {
-//     width: 7,
-//     height: 12,
-// };
+  const rectangle2 = {
+    width: 7,
+    height: 12,
+  };
 
-// console.log(rectangle1);
-// console.log(rectangle2);
+  console.log(rectangle1);
+  console.log(rectangle2);
 
-// console.log(getArea(rectangle1));
-// console.log(getArea(rectangle2));
+  console.log(getArea(rectangle1));
+  console.log(getArea(rectangle2));
+}
 
 // *****************js.classes*************************
 
-// class Rectangle {           // can be - const Rectangle = class {
-//     getArea() {
-//         return this.width * this.height;
-//     }
-//                             // can be - getArea = () => this.width * this.height;
-// }
+{
+  class Rectangle {
+    // can be - const Rectangle = class {
+    getArea() {
+      return this.width * this.height;
+    } // can be - getArea = () => this.width * this.height;
+  }
 
-// const rectangle1 = new Rectangle();
+  const rectangle1 = new Rectangle();
 
-// rectangle1.width = 5;
-// rectangle1.height = 6;
+  rectangle1.width = 5;
+  rectangle1.height = 6;
 
-// const rectangle2 = new Rectangle();
+  const rectangle2 = new Rectangle();
 
-// rectangle2.width = 7;
-// rectangle2.height = 12;
+  rectangle2.width = 7;
+  rectangle2.height = 12;
 
-// console.log(rectangle1);
-// console.log(rectangle2);
-// console.log(rectangle1.getArea());
-// console.log(rectangle2.getArea());
+  console.log(rectangle1);
+  console.log(rectangle2);
+  console.log(rectangle1.getArea());
+  console.log(rectangle2.getArea());
+}
 
 // ******************with constructor*******************
 
-class Rectangle {
+{
+  class Rectangle {
     constructor(width, height) {
-        this.width = width;
-        this.height = height;
+      this.width = width;
+      this.height = height;
     }
 
     getArea = () => this.width * this.height;
+  }
+
+  const rectangle1 = new Rectangle(5, 6);
+
+  const rectangle2 = new Rectangle(7, 12);
+
+  console.log(rectangle1);
+  console.log(rectangle2);
+
+  console.log(rectangle1.getArea());
+  console.log(rectangle2.getArea());
 }
-
-const rectangle1 = new Rectangle(5, 6);
-
-const rectangle2 = new Rectangle(7, 12);
-
-console.log(rectangle1);
-console.log(rectangle2);
-
-console.log(rectangle1.getArea());
-console.log(rectangle2.getArea());
-
-
