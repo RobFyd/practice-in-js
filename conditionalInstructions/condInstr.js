c6e1995e2298128be39abdb2f0f126b0718a0663
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-10-21 00:31:51 +0100

    conditional statements continued

--- conditionalInstructions/condInstr.js
+++ conditionalInstructions/condInstr.js
@@ -1,26 +1,36 @@
 {
-    let number1 = 25;
-    let number2 = 10;
-    let Result = "";
+  const number1 = 25;
+  const number2 = 10;
+  let Result = "";
 
-    if (number1 < number2) {
-        Result = "number1 is smaller than number2";
-    } else {
-        Result = "number1 is not smaller than number2";
-    }
+  if (number1 < number2) {
+    Result = "number1 is smaller than number2";
+  } else {
+    Result = "number1 is not smaller than number2";
+  }
 
-    let resultElement = document.getElementById("result");
+  const resultElement = document.getElementById("result");
 
-    resultElement.innerHTML = Result;
+  resultElement.innerHTML = Result;
 }
 
 {
-    let container = document.querySelector(".container");
-    console.log(container);
+  const container = document.querySelector(".container");
+  console.log(container);
 
-    if(container.classList.contains("dark")) {
-        container.style.backgroundColor = "darkslategrey";
-    } else {
-        container.style.backgroundColor = "lightgrey";
+  if (container.classList.contains("dark")) {
+    container.style.backgroundColor = "darkslategrey";
+  } else {
+    container.style.backgroundColor = "lightgrey";
+  }
+}
+
+{
+  const button = document.querySelector(".button");
+
+  button.addEventListener("click", () => {
+    if (button.innerText === "Dark mode ON") {
+      button.innerText = "Dark mode OFF";
     }
+  });
 }
