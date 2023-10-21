9068e3d414328bb7acd4894d52d4224886544d60
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-10-21 23:48:45 +0100

    C I continued

--- conditionalInstructions/condInstr.js
+++ conditionalInstructions/condInstr.js
@@ -66,3 +66,38 @@
 
   resultElement.innerHTML = Result;
 }
+
+{
+  let someText = "";
+  let Result;
+
+  if (someText) {
+    Result = "This is true";
+  } else {
+    Result = "This is false";
+  }
+
+  const resultElement = document.getElementById("text");
+
+  resultElement.innerHTML = Result;
+}
+
+{
+  let number1 = 20;
+  let number2 = 40;
+  let number3 = 30;
+
+  if (number1 > number2) {
+    if (number1 > number3) {
+      console.log("number1 is the biggest");
+    } else {
+      console.log("number3 is the biggest");
+    }
+  } else {
+    if (number2 > number3) {
+      console.log("number2 is the biggest");
+    } else {
+      console.log("number3 is the biggest");
+    }
+  }
+}
