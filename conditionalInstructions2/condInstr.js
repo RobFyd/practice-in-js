7e552d3dab4450a70d1823e2836c44ca6213ad2b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-10-24 00:18:28 +0100

    condInstr2 continued

--- conditionalInstructions2/condInstr.js
+++ conditionalInstructions2/condInstr.js
@@ -1,6 +1,16 @@
 const button = document.querySelector(".button");
 const body = document.querySelector(".body");
+let themeMode = document.querySelector(".themeMode");
+
+// toggle: if the class is there, remove it, if not, add it
+// add:  remove: if the class is there, remove it, if not, do nothing
 
 button.addEventListener("click", () => {
-  body.classList.add("dark");
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    themeMode.innerText = "OFF";
+  } else {
+    themeMode.innerText = "ON";
+  }
 });
