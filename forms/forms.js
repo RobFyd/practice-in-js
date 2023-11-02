c91e9e08ce2b4f051964a5830d9ac0f362231b55
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-02 23:44:29 +0000

    radio elements to js connected

--- forms/forms.js
+++ forms/forms.js
@@ -4,3 +4,9 @@ console.log(heightElement.value);
 console.log(heightElement.type);
 console.log(heightElement.step);
 console.log(heightElement.classList);
+
+
+const radioElement1 = document.querySelector(".js-radio-1");
+console.log(radioElement1);
+const radioElement2 = document.querySelector(".js-radio-2");
+console.log(radioElement2.checked = true);
