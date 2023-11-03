a520effe67e7e272ca343a818cc9253035c0e8e2
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-03 19:12:44 +0000

    Add input event listener to height input and fix form class name in HTML

--- forms/forms.js
+++ forms/forms.js
@@ -1,20 +1,22 @@
 const heightElement = document.querySelector(".js-height");
 console.log(heightElement);
-console.log(heightElement.value);
-console.log(heightElement.type);
-console.log(heightElement.step);
-console.log(heightElement.classList);
+// console.log(heightElement.value);
+// console.log(heightElement.type);
+// console.log(heightElement.step);
+// console.log(heightElement.classList);
+
+heightElement.addEventListener("input", () => {
+    console.log(`Yor height is ${heightElement.value}cm`);
+});
 
 const weightElement = document.querySelector(".js-weight");
 weightElement.focus();
 console.log(weightElement);
 
-
-const submit = document.querySelector("js-submit");
-
-
-
 const radioElement1 = document.querySelector(".js-radio-1");
 console.log(radioElement1);
 const radioElement2 = document.querySelector(".js-radio-2");
 console.log(radioElement2.checked = true);
+
+const submit = document.querySelector(".js-submit");
+const form = document.querySelector(".js-form");
