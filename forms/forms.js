e0020c34114f6c59169545afbf9a054554b39a64
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-04 00:03:11 +0000

    Form input validation and BMI calculation, include new functions and console logs added

--- forms/forms.js
+++ forms/forms.js
@@ -37,3 +37,32 @@ console.log((radioElement2.checked = true));
 formElement.addEventListener("reset", () => {
   console.log("Reset happens");
 });
+
+
+// formElement.addEventListener("input", (event) => {
+//   console.log("Submit done");
+
+//   let height = heightElement.value;
+//   let weight = weightElement.value;
+
+//   let bmi = weight / (height / 100) ** 2;
+
+//   console.log(height, weight, bmi);
+
+//   bmiElement.innerText = bmi.toFixed(1);
+// });
+
+if (5 > 8) {
+  console.log("5 is greater than 3");
+}
+else {
+  console.log("5 is not greater than 3");
+}
+
+function double(x) {
+  return x * 2;
+}
+
+const myNumber = 55;
+
+console.log(double(myNumber));
