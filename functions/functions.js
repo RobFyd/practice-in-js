cfe67188ebc409f8d39b11426299eef07a82316a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-05 21:07:49 +0000

    Fix formatting and arrow functions and variable declarations added

--- functions/functions.js
+++ functions/functions.js
@@ -1,4 +1,4 @@
-//Functions
+            //Functions
 
 {
   function double(number) {
@@ -8,39 +8,53 @@
   console.log(double(9));
 }
 
-// Syntax:
+            // Syntax:
 
 function name(parameter1, parameter2) {
-    //instructions
-    return value;
-  }
-  
-  {
-    function double(number) {
-      return number * 2;
-    }
-  
-    let myNumber = 55;
-    let doubled = double(120);
-  
-    console.log(doubled);
-    console.log(double(myNumber));
-  }
-  
-  // Arrow functions
-  
-  {
-    //   let double = (number) => {
-    //       return number * 2;
-    //   }                              //the same as below
-      let double = number => number * 2;
+  //instructions
+  return value;
+}
 
-      let myNumber = 33;
-
-      console.log(double(myNumber));
+{
+  function double(number) {
+    return number * 2;
   }
 
-  // let - block scope (variable)
-  // var - function scope (variable)
-  // const - block scope (constant)
-    
+  let myNumber = 55;
+  let doubled = double(120);
+
+  console.log(doubled);
+  console.log(double(myNumber));
+}
+
+            // Arrow functions
+
+{
+  //   let double = (number) => {
+  //       return number * 2;
+  //   }                              //the same as below
+  
+  let double = (number) => number * 2;
+
+  let myNumber = 33;
+
+  console.log(double(myNumber));
+}
+
+            // const - block scope (constant)
+            // let - block scope (variable)
+            // var - function scope (variable)
+
+{
+  let zmienna = 510;
+  console.log(zmienna);
+}
+// console.log(zmienna);
+
+function myFunction() {
+  var zmienna2 = 191;
+  console.log(zmienna2);
+}
+
+myFunction();
+// console.log(zmienna2);
