be2de38a56d7cc1dbdb8d5648574bff70a1e8142
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-05 20:10:45 +0000

    Function examples and arrow function syntax added

--- functions/functions.js
+++ functions/functions.js
@@ -8,3 +8,27 @@
   console.log(double(9));
 }
 
+// Syntax:
+
+function name(parameter1, parameter2) {
+    //instructions
+    return value;
+  }
+  
+  {
+    function double(number) {
+      return number * 2;
+    }
+  
+    let myNumber = 55;
+    let doubled = double(120);
+  
+    console.log(doubled);
+    console.log(double(myNumber));
+  }
+  
+  // Arrow functions
+  
+  {
+      let double = number => number * 2;  
+  }
