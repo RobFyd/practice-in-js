4ef43657bacf8a36d45e31dee8eb52f56d3b595c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-12 20:20:23 +0000

    Add HTML and JavaScript files for arrays, with sample code

---
+++ arrays/arrays.js
@@ -0,0 +1,14 @@
+// arays are used to store multiple values in a single variable
+
+const products = ["eggs", "milk", "butter", "bread", "cheese"];
+
+console.log(products[0]);
+console.log(products[1]);
+console.log(products[2]);
+console.log(products[3]);
+console.log(products[4]);
+
+products[2] = "cucumber";
+console.log(products);
+
+console.log(products.length);
