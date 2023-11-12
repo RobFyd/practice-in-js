59e5d115b6187d17bb0d1cea36bbcd1a9c16dd83
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-12 00:43:06 +0000

    variable declarations

--- functionAndVariablesTips/function2.js
+++ functionAndVariablesTips/function2.js
@@ -22,9 +22,9 @@
   var variable = 100; // undefined
 }
 
-// function in no name function( anonymous function )
-
 {
+  // function in no name function( anonymous function )
+
   (() => {
     welcome2(); // Hello World :)
 
@@ -36,4 +36,24 @@
 
     var variable2 = 100;
   })();
+}
+
+{
+    var variable3 = 100;
+
+    var variable3 = 200;
+
+    console.log(variable3); // 200
+}
+
+{
+    let result;
+
+    result = 20;
+
+    var myVariable;
+
+    myVariable = "text";
+
+    // const myConst; error // must be initialized
 }
