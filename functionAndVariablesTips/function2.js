786e0fa0f7755738909b92d38491360021b95482
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-12 13:54:40 +0000

    Refactor variables and add function examples

--- functionAndVariablesTips/function2.js
+++ functionAndVariablesTips/function2.js
@@ -39,21 +39,60 @@
 }
 
 {
-    var variable3 = 100;
+  var variable3 = 100;
 
-    var variable3 = 200;
+  var variable3 = 200;
 
-    console.log(variable3); // 200
+  console.log(variable3); // 200
 }
 
 {
-    let result;
+  let result;
 
-    result = 20;
+  result = 20;
 
-    var myVariable;
+  var myVariable;
 
-    myVariable = "text";
+  myVariable = "text";
 
-    // const myConst; error // must be initialized
+  // const myConst; error // must be initialized
+}
+
+{
+// several variables in one
+
+  let a = 3,
+    b = 6,
+    c = 9;
+  console.log(a, b, c);
+
+  const newDiv = document.createElement("div");
+  newDiv.textContent = `${b}, ${c}`; //or newDiv.textContent = b + ", " + c;
+  document.body.appendChild(newDiv);
+
+  const number = 5;
+  string = "text";
+  console.log(number, string);
+}
+
+{
+// default function parameters 
+
+  let power = (a, b = 1) => a ** b;
+  console.log(power(3, 3));
+  console.log(power(3));
+}
+
+{
+  // recursion - a function calls itself (rekurencja), (factorial - silnia)
+
+  let factorial = (number) => {
+    if (!number) {
+      return 1;
+    } else {
+      return number * factorial(number - 1);
+    }
+  };
+
+  console.log(factorial(3));
 }
