a35e1015217e171ce0aca66e07b683b97543a688
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-15 00:10:25 +0000

    form and styling added

--- To-do-list-exercisses/to-do.js
+++ To-do-list-exercisses/to-do.js
@@ -1,1 +1,24 @@
 console.log("test");
+
+{
+  const tasks = [
+    {
+      content: "example task 1",
+      done: false,
+    },
+    {
+      content: "example task 2",
+      done: true,
+    },
+  ];
+
+  const render = () => {
+    
+  };
+
+  const init = () => {
+
+  };
+
+  init();
+}
