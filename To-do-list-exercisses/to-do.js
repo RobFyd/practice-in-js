c045d6dcbaee921f4bee0bcb2251ab268352ff89
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-15 00:18:30 +0000

    Add task rendering functionality to to-do list

--- To-do-list-exercisses/to-do.js
+++ To-do-list-exercisses/to-do.js
@@ -13,11 +13,21 @@ console.log("test");
   ];
 
   const render = () => {
-    
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li>
+        ${task.content}
+        </li>
+    `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
   const init = () => {
-
+    render();
   };
 
   init();
