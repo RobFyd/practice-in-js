18d2222bba3bac82c7cbf5064ce466deba16b0df
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-26 23:49:05 +0000

    Add new elements and update object notation

--- objects2/objects2.js
+++ objects2/objects2.js
@@ -1,27 +1,45 @@
 // abbreviated notation
 
 {
-    const name = "Rob";
-const surname = "Soares";
+  const name = "Rob";
+  const surname = "Soares";
 
-const person = {
-  name,
-  surname,
-};
+  const person = {
+    name,
+    surname,
+  };
 
-console.log(person);
+  console.log(person);
 }
 
 // abbreviated notation: methods
 
 {
-    const person = {
-        name: "Rob",
-        sayHello() {
-            console.log(`Hello, my name is ${this.name}`);
-            document.querySelector(".js-hello").innerText = `hello! by ${this.name}`;
-        }
-    }
+  const person = {
+    name: "Rob",
+    sayHello() {
+      console.log(`Hello, my name is ${this.name}`);
+      document.querySelector(".js-hello").innerText = `hello! by ${this.name}`;
+      document.querySelector(".js-spanHello").innerHTML = `
+            <span class="hello__name">Nice to meet you. I am ${this.name}</span>
+            `;
+    },
+  };
 
-    person.sayHello();
+  person.sayHello();
+}
+
+// computed property names
+
+{
+  const currency = "EUR";
+
+  const exchangeData = {
+    USD: 3.6,
+    [currency]: 4.2,
+    GBP: 4.7,
+  };
+
+    console.log(exchangeData);
+    document.querySelector(".js-rates").innerText = `first rate: ${exchangeData.USD}\n second rate: ${exchangeData[currency]}\n third rate: ${exchangeData.GBP}`;
 }
