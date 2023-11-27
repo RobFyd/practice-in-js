27395999dfd394ba18758df5885bc26d198a71ad
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-27 23:49:57 +0000

    exercises continued

--- objects2/objects2.js
+++ objects2/objects2.js
@@ -58,4 +58,10 @@
 
   console.log(name, lastName, city, rest);
   document.querySelector(".js-details").innerText = `first detail: ${person.name}\n second detail: ${lastName}\n third detail: ${city}\n fourth detail: ${JSON.stringify(rest)}`; // JSON.stringify() converts a JavaScript object or value to a JSON string
+
+  const getPersonFullName = ({ name, surname }) => `${name} ${surname}`;
+  console.log(getPersonFullName(person));
+  document.querySelector(".js-fullName").innerHTML = `
+            <span>Nice to meet you. I am ${getPersonFullName(person)}</span>
+            `;
 }
