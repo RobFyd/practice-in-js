8393e471b6b586e367f2b9c538c11e4477a350ce
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-11-28 23:50:09 +0000

    merging arrays

--- arrays2/arrays2.js
+++ arrays2/arrays2.js
@@ -1,8 +1,20 @@
-
 // destructuring
 
-const numbers = [10, 22, 33, 44];
+{
+  const numbers = [10, 22, 33, 44];
 
-const [num1, num2, , num4, num5 = 0, ...rest] = numbers;
+  const [num1, num2, , num4, num5 = 0, ...rest] = numbers;
 
-console.log(num2, num4, num5, rest);
+  console.log(num2, num4, num5, rest);
+}
+
+// mergring arrays
+
+{
+    const numbers1 = [11, 22, 33];
+    const numbers2 = [44, 55, 66];
+    
+    const allNumbers = [...numbers1, ...numbers2];
+    
+    console.log(allNumbers);
+}
