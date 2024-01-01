ff32ac6a10bdb8536f2f64905737b7082ff5cfe1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-01 22:46:14 +0000

    anonymous function and best practise

--- events/events.js
+++ events/events.js
@@ -13,3 +13,19 @@ const hello2 = () => {
 const button = document.querySelector(".js-button");
 
 button.onclick = hello2;
+
+// 3. method - good practice, anonymous function
+
+const button2 = document.querySelector(".js-button2");
+
+button2.onclick = () => {
+    console.log("hello3");
+};
+
+// 4. method - best practice, addEventListener
+
+const button3 = document.querySelector(".js-button3");
+
+button3.addEventListener("click", () => {
+    console.log("hello4");
+});
