4d5483203736e87fd2e8feef23b6b3c295f6620a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-10 00:35:15 +0000

    date and time display refactored

--- date/date.js
+++ date/date.js
@@ -1,8 +1,24 @@
-function timeDate() {
-    var today = new Date();
-    var date = dzisiaj.toDateString();
-    var time = dzisiaj.toLocaleTimeString();
-    document.getElementById('dataczas').innerHTML = date + ' ' + time;
+const myFullDate = () => {
+    const today = new Date();
+    const date = today.toDateString();
+    const time = today.toLocaleTimeString();
+    document.getElementById("datetime").innerHTML = date + ' ' + time;
 }
 
-setInterval(timeDate, 1000);
+setInterval(myFullDate, 1000);
+
+const myTime = () => {
+    const today = new Date();
+    const time = today.toLocaleTimeString();
+    document.querySelector(".time__text").innerText = time;
+};
+
+setInterval(myTime, 1000);
+
+const myDate = () => {
+    const today = new Date();
+    const date = today.toDateString();
+    document.querySelector(".date__text").innerText = date;
+};
+
+setInterval(myDate, 1000);
