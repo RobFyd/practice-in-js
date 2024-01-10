12c7501b7d6f909e6b89e2a5f3de592810d45895
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-10 23:57:01 +0000

    Update date.html and date.js files

--- date/date.js
+++ date/date.js
@@ -1,32 +1,42 @@
 const myTime = () => {
-    const today = new Date();
-    const time = today.toLocaleTimeString();
-    document.querySelector(".js-timeText").innerText = time;
+  const today = new Date();
+  const time = today.toLocaleTimeString();
+  document.querySelector(".js-timeText").innerText = time;
 };
 
 setInterval(myTime, 1000);
 
 const myDate = () => {
-    const today = new Date();
-    const date = today.toDateString();
-    document.querySelector(".js-dateText").innerText = date;
+  const today = new Date();
+  const date = today.toDateString();
+  document.querySelector(".js-dateText").innerText = date;
 };
 
 setInterval(myDate, 1000);
 
 const myFullDate = () => {
-    const today = new Date();
-    const date = today.toDateString();
-    const time = today.toLocaleTimeString();
-    document.getElementById("js-datetime").innerHTML = date + ' ' + time;
-}
+  const today = new Date();
+  const date = today.toDateString();
+  const time = today.toLocaleTimeString();
+  document.getElementById("js-datetime").innerHTML = date + " " + time;
+};
 
 setInterval(myFullDate, 1000);
 
 const nextEx = () => {
-    const date = new Date();
-    const full = date.toLocaleDateString();
-    document.querySelector(".js-nextEx").innerText = full;
+  const date = new Date();
+  const full = date.toLocaleDateString();
+
+  document.querySelector(".js-nextEx").innerText = full;
 };
 
 setInterval(nextEx, 1000);
+
+const nextEx2 = () => {
+  const date = new Date();
+  const full2 = date.toString();
+
+  document.querySelector(".js-nextEx2").innerText = full2;
+};
+
+setInterval(nextEx2, 1000);
