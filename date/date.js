8f1132f35cf3678aa0db8ab88941bda630ca5dcf
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-10 00:41:35 +0000

    date and time display functionality added to date.html and date.js

--- date/date.js
+++ date/date.js
@@ -1,16 +1,7 @@
-const myFullDate = () => {
-    const today = new Date();
-    const date = today.toDateString();
-    const time = today.toLocaleTimeString();
-    document.getElementById("datetime").innerHTML = date + ' ' + time;
-}
-
-setInterval(myFullDate, 1000);
-
 const myTime = () => {
     const today = new Date();
     const time = today.toLocaleTimeString();
-    document.querySelector(".time__text").innerText = time;
+    document.querySelector(".js-timeText").innerText = time;
 };
 
 setInterval(myTime, 1000);
@@ -18,7 +9,16 @@ setInterval(myTime, 1000);
 const myDate = () => {
     const today = new Date();
     const date = today.toDateString();
-    document.querySelector(".date__text").innerText = date;
+    document.querySelector(".js-dateText").innerText = date;
 };
 
 setInterval(myDate, 1000);
+
+const myFullDate = () => {
+    const today = new Date();
+    const date = today.toDateString();
+    const time = today.toLocaleTimeString();
+    document.getElementById("js-datetime").innerHTML = date + ' ' + time;
+}
+
+setInterval(myFullDate, 1000);
