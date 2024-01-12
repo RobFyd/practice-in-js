034b205ae958a50193a692a3f90960a5a5b59fe6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-12 23:39:48 +0000

    timer functionality added

--- clock, timer/clock.js
+++ clock, timer/clock.js
@@ -8,6 +8,8 @@ document.querySelector(".js-button").addEventListener("click", () => {
       }, 2 * 1000);
 });
 
+////
+
 function showAlert() {
     alert("you clicked the second button");
 };
@@ -15,3 +17,46 @@ function showAlert() {
 document.querySelector(".js-button2").addEventListener("click", () => {
     setTimeout(showAlert, 500);
 });
+
+////
+
+const timeoutId = setTimeout(() => {
+    console.log("ALERT !!!");
+}, 5000);
+
+clearTimeout(timeoutId);  // cancel the timeout
+
+////
+
+const timeoutId2 = setTimeout(() => {
+    console.log("ALERT2 !!!");
+}, 5000);
+
+setTimeout(() => {
+    clearTimeout(timeoutId2);  // cancel the timeout after 2.5s
+}, 2500);
+
+///////////////////////////////////////////////
+
+setInterval(() => {
+    console.log("run every 1s");
+}, 1000);  // run every 1s
+
+////
+
+let counter = 0;
+
+setInterval(() => {
+    console.log("run every 500ms");
+    counter = counter + 1;              // increment counter
+}, 500);
+
+///
+
+const intervalId = setInterval(() => {
+    console.log("do whatever");
+}, 200);
+
+setInterval(() => {
+    clearInterval(intervalId);
+}, 1500);  // cancel the interval after 1.5s
