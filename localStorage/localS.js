b4a9ce5098329131a9553a43a19006cc7e4886b5
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-14 22:19:16 +0000

    cookies

--- localStorage/localS.js
+++ localStorage/localS.js
@@ -36,11 +36,23 @@ const products2 = [
 ];
 
 
-const productsToStorage2 = sessionStorage.setItem("products", JSON.stringify(products2));
+const productsToStorage2 = sessionStorage.setItem("products2", JSON.stringify(products2));
 console.log(productsToStorage2);
 
-const productsFromStorage2 = sessionStorage.getItem("products");
+const productsFromStorage2 = sessionStorage.getItem("products2");
 console.log(productsFromStorage2);
 
 const productsParsed2 = JSON.parse(productsFromStorage2);
 console.log(productsParsed2);
+
+
+// cookies are used to store data in the browser and it will stay there until we clear it out
+
+// header HTTP Set-Cookie
+// Set-Cookie: sessionId=38afes7a8; Max-Age=2592000
+
+document.cookie = "strawberry=cookie";
+console.log(document.cookie);
+
+document.cookie = "anotherCookie";
+console.log(document.cookie); 
