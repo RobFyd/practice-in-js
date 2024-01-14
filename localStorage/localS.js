dd615394e628ef12ecf73027829c484b872e8a7c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-14 21:33:31 +0000

    Add local storage functionality to store and retrieve products

--- localStorage/localS.js
+++ localStorage/localS.js
@@ -1,3 +1,5 @@
+// local storage is a way to store data in the browser and it will stay there until we clear it out
+
 const products = [
     {
         name: "leon parts",
@@ -8,3 +10,16 @@ const products = [
         price: 1,
     },
 ];
+
+
+const productsToStorage = localStorage.setItem("products", JSON.stringify(products));
+console.log(productsToStorage);
+
+const productsFromStorage = localStorage.getItem("products");
+console.log(productsFromStorage);
+
+const productsParsed = JSON.parse(productsFromStorage);
+console.log(productsParsed);
+
+
+// sessionStorage is the same as localStorage but it will be cleared out when we close the browser
