67d4653e8593e8d99ddb0203258151498f09d1c0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-29 23:58:22 +0000

    JSON exercisses

---
+++ JSON/json.js
@@ -0,0 +1,8 @@
+
+
+// JSON - JavaScript Object Notation
+
+//JSON.parse() - convert JSON string to JS object
+const data = JSON.parse('{"title": "Star Wars"}');
+
+console.log(data.parse('{"title": "Star Wars"}'));
