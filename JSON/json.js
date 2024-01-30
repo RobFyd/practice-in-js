586ebc3670fa927aa9dba70c606963295e6d5050
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-30 00:20:47 +0000

    JSON.parse and JSON.stringify exercises

--- JSON/json.js
+++ JSON/json.js
@@ -1,8 +1,24 @@
-
-
 // JSON - JavaScript Object Notation
 
 //JSON.parse() - convert JSON string to JS object
 const data = JSON.parse('{"title": "Star Wars"}');
 
-console.log(data.parse('{"title": "Star Wars"}'));
+try {
+  console.log(JSON.parse('{"title": "Star Wars"}'));
+} catch (error) {
+  console.log("Sorry but this is not a valid JSON string");
+}
+
+// checking if the string is valid JSON
+
+const isValidJson = (json) => {
+  try {
+    JSON.parse(json);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// JSON.stringify() - convert JS object to JSON string
+const jsonString = JSON.stringify({ title: "Star Wars" });
