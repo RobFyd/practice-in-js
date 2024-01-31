32194c69ec8996fcdae4faaaf0b3903019d60536
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-01-31 00:53:31 +0000

    post request added

--- axios/axios.js
+++ axios/axios.js
@@ -1,6 +1,17 @@
 // import axios from "axios";
 // instead import - cdn link for axios in .html
 
-axios.get("products.json")
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error));
+// get request
+axios
+  .get("products.json")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.log(error));
+
+// post request
+axios
+  .post("/add-person", {
+    name: "Max",
+    age: 28,
+  })
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
