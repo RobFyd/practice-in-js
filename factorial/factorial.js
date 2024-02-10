1ee7f61331ce14e6ed0e83d32ce67bbff54fe2fd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-10 15:51:32 +0000

    iterative approach factorial added

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,5 +1,6 @@
 console.log('factorial.js loaded');
 
+// factorial of n
 n = 7;
 
 const factorial = (n) => {
@@ -11,4 +12,17 @@ const factorial = (n) => {
     return n * factorial(n - 1);
 };
 
-console.log(factorial(n));
+console.log("n! = ", factorial(n));
+
+// factorial of m using iterative approach
+m = 6;
+
+const factorialIterative = (m) => {
+    let result = 1;
+    for (let i = 1; i <= m; i++) {
+        result *= i;
+    }
+    return result;
+};
+
+console.log("m! = ", factorialIterative(m));
