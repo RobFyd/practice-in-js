72bac8e353108969fc4da0f4019336c01b9997e9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-10 15:39:02 +0000

    Add factorial.html and factorial.js files

---
+++ factorial/factorial.js
@@ -0,0 +1,14 @@
+console.log('factorial.js loaded');
+
+n = 7;
+
+const factorial = (n) => {
+    // base case
+    if (n === 0) {
+        return 1;
+    }
+    // recursive case
+    return n * factorial(n - 1);
+};
+
+console.log(factorial(n));
