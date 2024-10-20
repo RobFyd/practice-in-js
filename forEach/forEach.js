e3890279536c31dec5c29f2bf970bee206ab0f9e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-20 23:59:41 +0100

    Add forEach.html and forEach.js files

---
+++ forEach/forEach.js
@@ -0,0 +1,12 @@
+const people = [
+  { name: "John", lastName: "Doe", country: "USA", salary: 1000 },
+  { name: "Jane", lastName: "Jab", country: "UK", salary: 2000 },
+  { name: "Ivan", lastName: "Ivanov", country: "RU", salary: 3000 },
+  { name: "Alan", lastName: "Ford", country: "USA", salary: 4000 },
+  { name: "Robert", lastName: "Sydrych", country: "PL", salary: 5000 },
+  { name: "Stefan", lastName: "Kowalski", country: "PL", salary: 6000 },
+  { name: "Lee", lastName: "Chin", country: "CN", salary: 7000 },
+  { name: "Mao", lastName: "Zedong", country: "CN", salary: 8000 },
+  { name: "Vladimir", lastName: "Srutin", country: "RU", salary: 9000 },
+  { name: "Edward", lastName: "Snowden", country: "UK", salary: 10000 },
+];
